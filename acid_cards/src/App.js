87570b4d1312fe4e434fd669b94a6e4e21370d43
Anastasia Deryabkina <der_anastasi84@mail.ru>
2023-05-22 22:29:49 +0700

    change: Navbar component description separated

--- acid_cards/src/App.js
+++ acid_cards/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Navbar from './components/Navbar/Navbar'
+
 function App() {
   return (
     <div className="App">
@@ -9,24 +11,7 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
         <main className='main'>
-          <nav className='navbar'>
-            <ul type="none">
-              <li><a href='#'>Ксенон  Xe </a>(2)</li>
-              <li><a href='#'>Фтор    F  </a>(2)</li>
-              <li><a href='#'>Хлор    Cl </a>(5)</li>
-              <li><a href='#'>Бром    Br </a>(5)</li>
-              <li><a href='#'>Йод     I  </a>(7)</li>
-              <li><a href='#'>Сера    S  </a>(11)</li>
-              <li><a href='#'>Селен   Se </a>(3)</li>
-              <li><a href='#'>Теллур  Te </a>(3)</li>
-              <li><a href='#'>Азот    N  </a>(5)</li>
-              <li><a href='#'>Фосфор  P  </a>(9)</li>
-              <li><a href='#'>Мышьяк  As </a>(2)</li>
-              <li><a href='#'>Сурьма  Sb </a>(2)</li>
-              <li><a href='#'>Кремний Si </a>(3)</li>
-              <li><a href='#'>Бор     B  </a>(1)</li>
-            </ul>
-          </nav>
+          <Navbar />
           <article></article>
         </main>
       </div>
