559e88abb706a0989244927e3ab1cd1d90514f51
Anastasia Deryabkina <der_anastasi84@mail.ru>
2023-05-23 15:03:13 +0700

    change: route way add: auto generation of few articles

--- acid_cards/src/components/Main/Article/Articles.jsx
+++ acid_cards/src/components/Main/Article/Articles.jsx
@@ -2,28 +2,18 @@
 import styles from './Article.module.css'
 
 
-import * as React from "react";
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-    {
-        path: "/xenon",
-        element: <div className={styles.acid_cards}>
+export function Xsenon () {
+    return (
+        <div className={styles.acid_cards} >
             <h1>Ксенон Xe</h1>
             <section></section>
-        </div>,
-    },
-    {
-        path: "/ftor",
-        element: <div className={styles.acid_cards}>
+        </div >)
+};
+
+export function Ftor () {
+    return (
+        <div className={styles.acid_cards} >
             <h1>Фтор F</h1>
             <section></section>
-        </div>,
-    },
-]);
-
-
-export default router;
+        </div >)
+};
--- acid_cards/src/components/Main/Main.jsx
+++ acid_cards/src/components/Main/Main.jsx
@@ -2,20 +2,41 @@
 import styles from './Main.module.css'
 
 import Navbar from './Navbar/Navbar'
-import router from './Article/Articles'
+import { Xsenon, Ftor } from './Article/Articles'
 
 import * as React from "react";
-import { RouterProvider } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
+
+const FormArticle = () => {
+    const url = window.location.href
+
+    let elem;
+    if (url.includes('/'))
+        elem = url.split('/').pop()
+    else
+        elem = url.split('\\').pop()
+
+    console.log(elem)
+
+    return (
+        <div>
+            <h1>{elem}</h1>
+            <section></section>
+        </div >)
+}
 
 const Main = () => {
     return (
         <main className={styles.main}>
             <Navbar />
             <div class="articles_wrapper_content">
-                <React.StrictMode>
-                    <RouterProvider router={router} />
-                </React.StrictMode>
+                {/* <FormArticle name="xenon" /> */}
+                <Routes>
+                    <Route path="/xenon"    element={<FormArticle />} />
+                    <Route path="/ftor"     element={<Ftor />} />
+                    <Route path="/chlor"    element={<FormArticle />} />
+                </Routes>
             </div>
         </main>
     )
