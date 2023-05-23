59dfb427f9290fdd386301f714c61f93bfcc2c37
Anastasia Deryabkina <der_anastasi84@mail.ru>
2023-05-23 14:59:41 +0700

    change: <a> to <NavLink>

--- acid_cards/src/components/Main/Navbar/Navbar.jsx
+++ acid_cards/src/components/Main/Navbar/Navbar.jsx
@@ -1,27 +1,29 @@
 
+import { NavLink } from 'react-router-dom';
 import styles from './Navbar.module.css'
 
 
 const Navbar = () => {
+
     return <nav className={styles.navbar}>
         <h4>Элементы</h4>
         <ul type="none">
-            <li><a href='/xenon'>   Ксенон  Xe</a>  (2)</li>
-            <li><a href='/ftor'>    Фтор    F</a>   (2)</li>
-            <li><a href='/chlor'>   Хлор    Cl</a>  (5)</li>
-            <li><a href='/brom'>    Бром    Br</a>  (5)</li>
-            <li><a href='/iod'>     Йод     I</a>   (7)</li>
-            <li><a href='/sera'>    Сера    S</a>   (11)</li>
-            <li><a href='/selen'>   Селен   Se</a>  (3)</li>
-            <li><a href='/tellur'>  Теллур  Te</a>  (3)</li>
-            <li><a href='/azot'>    Азот    N</a>   (5)</li>
-            <li><a href='/phosphor'>Фосфор  P</a>   (9)</li>
-            <li><a href='/mishyak'> Мышьяк  As</a>  (2)</li>
-            <li><a href='/surma'>   Сурьма  Sb</a>  (2)</li>
-            <li><a href='/kremniy'> Кремний Si</a>  (3)</li>
-            <li><a href='/bor'>     Бор     B</a>   (1)</li>
+            <li><NavLink to='/xenon'>   Ксенон  Xe  </NavLink>  (2)</li>
+            <li><NavLink to='/ftor'>    Фтор    F   </NavLink>  (2)</li>
+            <li><NavLink to='/chlor'>   Хлор    Cl  </NavLink>  (5)</li>
+            <li><NavLink to='/brom'>    Бром    Br  </NavLink>  (5)</li>
+            <li><NavLink to='/iod'>     Йод     I   </NavLink>  (7)</li>
+            <li><NavLink to='/sera'>    Сера    S   </NavLink>  (11)</li>
+            <li><NavLink to='/selen'>   Селен   Se  </NavLink>  (3)</li>
+            <li><NavLink to='/tellur'>  Теллур  Te  </NavLink>  (3)</li>
+            <li><NavLink to='/azot'>    Азот    N   </NavLink>  (5)</li>
+            <li><NavLink to='/phosphor'>Фосфор  P   </NavLink>  (9)</li>
+            <li><NavLink to='/mishyak'> Мышьяк  As  </NavLink>  (2)</li>
+            <li><NavLink to='/surma'>   Сурьма  Sb  </NavLink>  (2)</li>
+            <li><NavLink to='/kremniy'> Кремний Si  </NavLink>  (3)</li>
+            <li><NavLink to='/bor'>     Бор     B   </NavLink>  (1)</li>
         </ul>
-    </nav>
+    </nav >
 }
 
 export default Navbar;
