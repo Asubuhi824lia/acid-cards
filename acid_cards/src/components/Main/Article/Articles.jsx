23f7f37f8f5b11c163f129d08fd5f19e6185125d
Anastasia Deryabkina <der_anastasi84@mail.ru>
2023-05-29 18:35:39 +0700

    change: FormArticle moved to a separate file

--- acid_cards/src/components/Main/Article/Articles.jsx
+++ acid_cards/src/components/Main/Article/Articles.jsx
@@ -1,19 +1,16 @@
 
 import styles from './Article.module.css'
 
+const FormArticle = () => {
+    const url = window.location.href
 
-export function Xsenon () {
+    let elem = url.split('/').pop()
+
     return (
-        <div className={styles.acid_cards} >
-            <h1>Ксенон Xe</h1>
+        <div>
+            <h1>{elem}</h1>
             <section></section>
         </div >)
-};
+}
 
-export function Ftor () {
-    return (
-        <div className={styles.acid_cards} >
-            <h1>Фтор F</h1>
-            <section></section>
-        </div >)
-};
+export default FormArticle;
--- acid_cards/src/components/Main/Main.jsx
+++ acid_cards/src/components/Main/Main.jsx
@@ -2,49 +2,24 @@
 import styles from './Main.module.css'
 
 import Navbar from './Navbar/Navbar'
-import { Xsenon, Ftor } from './Article/Articles'
+import FormArticle from './Article/Articles'
 
 import * as React from "react";
 import { Route, Routes } from "react-router-dom";
 
+
 import acid_elems from "../../acid_elems.json"
-
-
-const FormArticle = () => {
-    const url = window.location.href
-
-    let elem;
-    if (url.includes('/'))
-        elem = url.split('/').pop()
-    else
-        elem = url.split('\\').pop()
-
-    console.log(elem)
-
-    return (
-        <div>
-            <h1>{elem}</h1>
-            <section></section>
-        </div >)
-}
-
-const formRoute = (elem) => {
-    const path = "/" + elem;
-    return <Route path={path} element={<FormArticle />} />
-}
 
 
 const Main = () => {
 
-    let routes = acid_elems.elems.map(element => {
-        return formRoute(element.name_en.toLowerCase())
-    });
+    let routes = acid_elems.elems
+        .map(e => <Route path={"/" + e.name_en.toLowerCase()} element={<FormArticle/>} />);
 
     return (
         <main className={styles.main}>
             <Navbar />
-            <div class="articles_wrapper_content">
-                {/* <FormArticle name="xenon" /> */}
+            <div className="articles_wrapper_content">
                 <Routes>
                     {routes}
                 </Routes>
