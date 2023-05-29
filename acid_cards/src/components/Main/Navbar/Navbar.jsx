a058a507bb4d3555fbb96bd2ebd6ac9bbf1498f1
Anastasia Deryabkina <der_anastasi84@mail.ru>
2023-05-29 14:39:07 +0700

    add: generation a list of navlinks

--- acid_cards/src/components/Main/Navbar/Navbar.jsx
+++ acid_cards/src/components/Main/Navbar/Navbar.jsx
@@ -3,25 +3,31 @@ import { NavLink } from 'react-router-dom';
 import styles from './Navbar.module.css'
 
 
+import acid_elems from "../../../acid_elems.json"
+
+
 const Navbar = () => {
+
+    const formNavLink = (str_en, str_ru, symbol, number) => {
+        return <li><NavLink to={str_en}> {str_ru} {symbol}</NavLink> ({number})</li>
+    }
+
+    let NavLinks = []
+    acid_elems.elems.forEach(element => {
+        const str_en = element.name_en.toLowerCase()
+        const str_ru = element.name_ru[0].toUpperCase() + element.name_ru.slice(1).toLowerCase()
+        const symbol = element.symbol
+        const number = element.acids.quantity
+
+        const li = formNavLink(str_en, str_ru, symbol, number)
+        NavLinks.push(li)
+    });
+    console.log(NavLinks)
 
     return <nav className={styles.navbar}>
         <h4>Элементы</h4>
         <ul type="none">
-            <li><NavLink to='/xenon'>   Ксенон  Xe  </NavLink>  (2)</li>
-            <li><NavLink to='/ftor'>    Фтор    F   </NavLink>  (2)</li>
-            <li><NavLink to='/chlor'>   Хлор    Cl  </NavLink>  (5)</li>
-            <li><NavLink to='/brom'>    Бром    Br  </NavLink>  (5)</li>
-            <li><NavLink to='/iod'>     Йод     I   </NavLink>  (7)</li>
-            <li><NavLink to='/sera'>    Сера    S   </NavLink>  (11)</li>
-            <li><NavLink to='/selen'>   Селен   Se  </NavLink>  (3)</li>
-            <li><NavLink to='/tellur'>  Теллур  Te  </NavLink>  (3)</li>
-            <li><NavLink to='/azot'>    Азот    N   </NavLink>  (5)</li>
-            <li><NavLink to='/phosphor'>Фосфор  P   </NavLink>  (9)</li>
-            <li><NavLink to='/mishyak'> Мышьяк  As  </NavLink>  (2)</li>
-            <li><NavLink to='/surma'>   Сурьма  Sb  </NavLink>  (2)</li>
-            <li><NavLink to='/kremniy'> Кремний Si  </NavLink>  (3)</li>
-            <li><NavLink to='/bor'>     Бор     B   </NavLink>  (1)</li>
+            {NavLinks}
         </ul>
     </nav >
 }
